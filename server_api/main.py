dba2cb5767bf45e23554c899caa7f50336beb3ec
Gabe Busto <gbusto.gator@gmail.com>
2025-04-04 13:01:30 -0400

    minor update to have main.py use the right PORT var

--- server_api/main.py
+++ server_api/main.py
@@ -98,4 +98,4 @@ async def generate_tts(request: TTSRequest, api_key: str = Depends(get_api_key))
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
+    uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT"), reload=True) 
